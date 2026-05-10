3651ec3731c3c38e4a25ff4316a3b7298ec95459
AJDestroyer20 <alfonso.j.gomez.m@icloud.com>
2026-05-10 11:56:36 -0600

    pienso quitar este script

--- setup/windows-msvc.bat
+++ setup/windows-msvc.bat
@@ -6,18 +6,5 @@ curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771
 vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
 del vs_Community.exe
 
-echo Installing scripting and engine libraries
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install grig.audio
-haxelib install tink_core
-haxelib install tjson
-haxelib install hscript
-haxelib install hscript-iris
-haxelib install hxvlc
-haxelib install linc_luajit
-haxelib install discord_rpc
-haxelib install away3d 5.0.9
-
 echo Finished.
 pause
